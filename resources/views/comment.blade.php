f7c5c9590f7035ae29f9f40ce6dbefcdd2f37960
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-06-09 14:34:02 +0700

    feat: implement CommentComponent for managing comments with edit functionality

---
+++ resources/views/comment.blade.php
@@ -0,0 +1,73 @@
+@php
+    $isEditMode = $this->isEditMode();
+    $isEditable = $this->isEditable();
+@endphp
+<div
+    class="fi-in-repeatable-item block rounded-xl bg-white p-4 shadow-sm ring-1 ring-gray-950/5 dark:bg-white/5 dark:ring-white/10">
+    <div class="flex gap-x-3">
+        @if (config('filament-comments.display_avatars'))
+            <x-filament-panels::avatar.user size="md" :user="$comment->user" />
+        @endif
+
+        <div class="flex-grow space-y-2 pt-[6px]">
+            <div class="flex gap-x-2 items-center justify-between">
+                <div class="flex gap-x-2 items-center">
+                    <div class="text-sm font-medium text-gray-950 dark:text-white">
+                        {{ $comment->user[config('filament-comments.user_name_attribute')] }}
+                    </div>
+
+                    <div class="text-xs font-medium text-gray-400 dark:text-gray-500">
+                        {{ $comment->created_at->diffForHumans() }}
+                    </div>
+                </div>
+
+                <div class="flex items-center gap-2">
+
+                    @if (auth()->user()->can('update', $comment))
+                        <div class="flex-shrink-0">
+                            @if ($isEditable && !$isEditMode)
+                                <x-filament::icon-button wire:click="edit({{ $comment->id }})"
+                                    icon="{{ config('filament-comments.icons.edit') }}" color="warning"
+                                    tooltip="{{ __('filament-comments::filament-comments.comments.edit.tooltip') }}" />
+                            @endif
+                        </div>
+                    @endif
+
+                    @if (auth()->user()->can('delete', $comment))
+                        <div class="flex-shrink-0">
+                            <x-filament::icon-button wire:click="delete"
+                                icon="{{ config('filament-comments.icons.delete') }}" color="danger"
+                                tooltip="{{ __('filament-comments::filament-comments.comments.delete.tooltip') }}" />
+                        </div>
+                    @endif
+                </div>
+            </div>
+            @if ($isEditMode)
+                <div>
+                    {{ $this->form }}
+
+                    <div class="flex items-center gap-2 mt-4">
+                        <x-filament::button wire:click="update" color="primary">
+                            {{ __('filament-comments::filament-comments.edit-comment.save') }}
+                        </x-filament::button>
+                        <x-filament::button wire:click="cancelEdit" color="warning">
+                            {{ __('filament-comments::filament-comments.edit-comment.cancel') }}
+                        </x-filament::button>
+                    </div>
+                </div>
+            @else
+                <div
+                    class="prose dark:prose-invert [&>*]:mb-2 [&>*]:mt-0 [&>*:last-child]:mb-0 prose-sm text-sm leading-6 text-gray-950 dark:text-white">
+                    @if (config('filament-comments.editor') === 'markdown')
+                        {{ Str::of($comment->comment)->markdown()->toHtmlString() }}
+                    @else
+                        {{ Str::of($comment->comment)->toHtmlString() }}
+                    @endif
+                </div>
+                @if ($comment->is_edited)
+                    <p class="text-xs text-gray-400">{{ __('filament-comments::filament-comments.edit-comment.edited') }}</p>
+                @endif
+            @endif
+        </div>
+    </div>
+</div>
---
+++ src/Livewire/CommentComponent.php
@@ -0,0 +1,166 @@
+<?php
+namespace Evitenic\FilamentComments\Livewire;
+
+use Carbon\Carbon;
+use Evitenic\FilamentComments\Models\FilamentComment;
+use Evitenic\FilamentComments\Traits\HasEditorComponent;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Model;
+use Livewire\Component;
+
+class CommentComponent extends Component implements HasForms
+{
+    use InteractsWithForms, HasEditorComponent;
+
+    public ?array $newData = [];
+
+    public $editCommentId = null;
+
+    public Model $record;
+
+    public FilamentComment $comment;
+
+    public ?int $editTimeLimit = null;
+
+    public ?Carbon $editExpirationTime = null;
+
+    public function mount(): void
+    {
+        $this->editTimeLimit = config('filament-comments.edit_time_limit', null);
+        if ($this->editTimeLimit === null || $this->editTimeLimit <= 0) {
+            $this->editExpirationTime = null;
+        } else {
+            $this->editExpirationTime = $this->comment->created_at->addMinutes($this->editTimeLimit);
+        }
+        $this->form->fill();
+    }
+
+    public function form(Form $form): Form
+    {
+        if (! auth()->user()->can('update', $this->comment)) {
+            return $form;
+        }
+
+        $editor = $this->getEditorComponent(config('filament-comments.editor'));
+
+        return $form
+            ->schema([
+                $editor,
+            ])
+            ->statePath('newData');
+    }
+
+    public function isEditable(): bool
+    {
+        if (! $this->editExpirationTime) {
+            return true;
+        }
+
+        $isExp = now()->lessThan($this->editExpirationTime);
+        return $isExp;
+    }
+
+    public function isEditMode(): bool
+    {
+        return $this->editCommentId && $this->editCommentId === $this->comment->id;
+    }
+
+    public function edit(int $id): void
+    {
+        $isEditable = $this->isEditable();
+
+        if (! $isEditable) {
+            Notification::make()
+                ->title(__('filament-comments::filament-comments.edit-comment.expired', ['minute' => $this->editTimeLimit]))
+                ->warning()
+                ->send();
+
+            $this->cancelEdit();
+            return;
+        }
+
+        $this->form->fill([
+            'comment' => $this->record->filamentComments()->find($id)->comment,
+        ]);
+        $this->editCommentId = $id;
+    }
+
+    public function cancelEdit(): void
+    {
+        $this->editCommentId = null;
+        $this->form->fill();
+    }
+
+    public function update(): void
+    {
+        $comment = $this->comment;
+
+        if (! auth()->user()->can('update', $comment)) {
+            return;
+        }
+
+        $isEditable = $this->isEditable();
+
+        if (! $isEditable) {
+            Notification::make()
+                ->title(__('filament-comments::filament-comments.edit-comment.expired', ['minute' => $this->editTimeLimit]))
+                ->warning()
+                ->send();
+            $this->cancelEdit();
+            return;
+        }
+
+        $data = $this->form->getState();
+        if ($comment->comment === $data['comment']) {
+            $this->cancelEdit();
+            return;
+        }
+
+        $this->form->validate();
+
+        $data = $this->form->getState();
+
+        $comment->update([
+            'comment'   => $data['comment'],
+            'is_edited' => true,
+        ]);
+
+        Notification::make()
+            ->title(__('filament-comments::filament-comments.edit-comment.success'))
+            ->success()
+            ->send();
+
+        $this->cancelEdit();
+    }
+
+    public function delete(): void
+    {
+        $comment = $this->comment;
+
+        if (! $comment) {
+            return;
+        }
+
+        if (! auth()->user()->can('delete', $comment)) {
+            return;
+        }
+
+        $comment->delete();
+
+        $this->dispatch('refreshComments');
+
+        Notification::make()
+            ->title(__('filament-comments::filament-comments.notifications.deleted'))
+            ->success()
+            ->send();
+    }
+
+    public function render(): View
+    {
+        return view('filament-comments::comment');
+    }
+}
